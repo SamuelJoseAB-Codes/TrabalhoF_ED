4c071eed4f30f9b32a8ff9fbbaf5a19d255c8541
samuel <samuelbisneto10@gmail.com>
2023-06-28 11:25:59 -0300

    sapecagens atualizadas

--- Main.java
+++ Main.java
@@ -7,6 +7,114 @@ public class Main {
                 new Map();
             }
         });
+        Game game = new Game(); // Inicializa a classe Game
+
+        // Define os valores iniciais dos atributos
+        game.setMoedasDTranporte(3);
+        game.setMaxwell("Maxwell");
+
+        // Exibe os valores iniciais dos atributos
+        System.out.println("Moedas de Transporte: " + game.getMoedasDTranporte());
+        System.out.println("Nome do Personagem: " + game.getMaxwell());
+        Mercador mercador = new Mercador();
+        mercador.fazerPerguntas();
+        mercador.exibirInformacoes();
+        // Iniciar as cidades do jogo
+        City ubud = new City("Ubud");
+        City cidade2 = new City("kingdom of legmond");
+        City cidade3 = new City("Principality of Nekikh");
+        City cidade4 = new City("Principality of Gritesthr");
+        City cidade5 = new City("Protectorate of Dogrove");
+        City cidade6 = new City("Kingdom of Lastwatch");
+        City cidade7 = new City("Grand Duchy of Smalia");
+        City cidade8 = new City("Kingdom of Oldcalia");
+        City cidade9 = new City("Kingdom of Kalb");
+        City cidade10 = new City("Aymar League");
+        City cidade11 = new City("Defalsia");
+        City cidade12 = new City("Vunese of Empire");
+        City cidade13 = new City("Principality of Karhora");
+        City cidade14 = new City("Chandir Sultanate");
+        City cidade15 = new City("Bun ");
+        City cidade16 = new City("Principality of Kasya");
+        City cidade17 = new City("Nargumun");
+
+        Graph grafo = new Graph();
+
+        grafo.insertVertice(ubud);
+        grafo.insertVertice(cidade2);
+        grafo.insertVertice(cidade3);
+        grafo.insertVertice(cidade4);
+        grafo.insertVertice(cidade5);
+        grafo.insertVertice(cidade6);
+        grafo.insertVertice(cidade7);
+        grafo.insertVertice(cidade8);
+        grafo.insertVertice(cidade9);
+        grafo.insertVertice(cidade10);
+        grafo.insertVertice(cidade11);
+        grafo.insertVertice(cidade12);
+        grafo.insertVertice(cidade13);
+        grafo.insertVertice(cidade14);
+        grafo.insertVertice(cidade15);
+        grafo.insertVertice(cidade16);
+        grafo.insertVertice(cidade17);
+
+        //abud
+        grafo.insertAresta(ubud, cidade2);
+        grafo.insertAresta(ubud, cidade3);
+
+        //legmond
+        grafo.insertAresta(cidade2, cidade3);
+        grafo.insertAresta(cidade2, cidade8);
+        grafo.insertAresta(cidade2, cidade5);
+
+        //nekikh
+        grafo.insertAresta(cidade3, cidade4);
+
+        //gritestar
+        grafo.insertAresta(cidade4, cidade5);
+        grafo.insertAresta(cidade4, cidade6);
+
+        //doggrove
+        grafo.insertAresta(cidade5, cidade6);
+        grafo.insertAresta(cidade5, cidade8);
+
+        grafo.insertAresta(cidade6, cidade7);
+        
+        grafo.insertAresta(cidade8, cidade6);
+        grafo.insertAresta(cidade8, cidade7);
+        grafo.insertAresta(cidade8, cidade9);
+        grafo.insertAresta(cidade8, cidade10);
+        grafo.insertAresta(cidade8, cidade11);
+
+        grafo.insertAresta(cidade9, cidade10);
+        grafo.insertAresta(cidade9, cidade12);
+
+        grafo.insertAresta(cidade9, cidade12);
+
+        grafo.insertAresta(cidade11, cidade10);
+
+        grafo.insertAresta(cidade10, cidade12);
+        grafo.insertAresta(cidade10, cidade13);
+        grafo.insertAresta(cidade10, cidade14);
+        grafo.insertAresta(cidade10, cidade15);
+        grafo.insertAresta(cidade10, cidade17);
+
+        grafo.insertAresta(cidade13, cidade17);
+
+        grafo.insertAresta(cidade14, cidade15);
+        grafo.insertAresta(cidade14, cidade16);
+
+        grafo.insertAresta(cidade15, cidade17);
+        
+
+
+
+        Vertice currentCity = grafo.get(ubud);
+
+        for (int x = 0; x < currentCity.getSize(); x++) {
+            System.out.println(x + "\t" + currentCity.getVertice(x).getEnd().getCity().getName());
+        }
+
 
         
     }
--- Mercador.java
+++ Mercador.java
@@ -1,26 +1,43 @@
-
 import java.util.Scanner;
 
 public class Mercador {
-    public void Perguntas(){
+    private int numMoedas;
+    private String origem;
+    private String destino;
+    private boolean trocarMoedasPorLimiar;
+    private boolean distanciaMenorQue3;
+
+    public Mercador() {
+        this.numMoedas = 0;
+        this.origem = "";
+        this.destino = "";
+        this.trocarMoedasPorLimiar = false;
+        this.distanciaMenorQue3 = false;
+    }
+
+    public void fazerPerguntas() {
         Scanner scanner = new Scanner(System.in);
-        
+
         System.out.println("Quantas moedas de transporte você tem?");
-        int numMoedas = scanner.nextInt();
-        
-        System.out.println("Distância é menor que 3? (Responda com 'sim' ou 'não')");
-        boolean distanciaMenorQue3 = scanner.next().equalsIgnoreCase("sim");
-        
+        this.numMoedas = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("De onde você vem?");
+        this.origem = scanner.nextLine();
+
+        System.out.println("Para onde você vai?");
+        this.destino = scanner.nextLine();
+
         System.out.println("Deseja trocar suas moedas por limiar na jóia? (Responda com 'sim' ou 'não')");
-        boolean trocarMoedasPorLimiar = scanner.next().equalsIgnoreCase("sim");
-        
-        int recompensa = calcularRecompensa(numMoedas, distanciaMenorQue3, trocarMoedasPorLimiar);
-        System.out.println("Recompensa: " + recompensa + " moedas");
+        this.trocarMoedasPorLimiar = scanner.nextLine().equalsIgnoreCase("sim");
+
+        System.out.println("Distância é menor que 3? (Responda com 'sim' ou 'não')");
+        this.distanciaMenorQue3 = scanner.nextLine().equalsIgnoreCase("sim");
 
         scanner.close();
-      }
+    }
 
-       public static int calcularRecompensa(int numMoedas, boolean distanciaMenorQue3, boolean trocarMoedasPorLimiar) {
+    public int calcularRecompensa() {
         if (numMoedas < 5) {
             if (distanciaMenorQue3) {
                 if (trocarMoedasPorLimiar) {
@@ -58,5 +75,13 @@ public class Mercador {
                 }
             }
         }
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Quantidade de moedas: " + numMoedas);
+        System.out.println("Origem: " + origem);
+        System.out.println("Destino: " + destino);
+        System.out.println("Trocar moedas por limiar na jóia: " + (trocarMoedasPorLimiar ? "Sim" : "Não"));
+        System.out.println("Recompensa: " + calcularRecompensa() + " moedas");
     }
 }
